c20c6017fb19a1874877f021ec2cc3885b92df78
hbruckman <henry.bruckman@gmail.com>
2026-03-17 12:08:40 -0400

    Add HttpMiddleware

---
+++ src/Shared/Http/HttpMiddleware.cs
@@ -0,0 +1,7 @@
+namespace Shared.Http;
+
+using System.Collections;
+using System.Net;
+
+public delegate Task HttpMiddleware(HttpListenerRequest req,
+	HttpListenerResponse res, Hashtable props, Func<Task> next);
